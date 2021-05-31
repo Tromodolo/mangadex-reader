3f2500fd655da9ab2e77735f0954764846d2b48a
Filip Ekström <Filip.ekstrom98@gmail.com>
2021-05-31 19:38:02 +0200

    Increase search limit to 20

--- app/Api/SearchManga.ts
+++ app/Api/SearchManga.ts
@@ -5,7 +5,7 @@ import RemoveToken from './RateLimit';
 // This, like FetchMangaDexAtHome should not be cached because it just doesn't work
 async function SearchManga(name: string, offset: number = 0): Promise<MangaList> {
 	await RemoveToken(1);
-	const res = await fetch(`${ApiRoot}/manga?title=${name}&offset=${offset}&limit=5`);
+	const res = await fetch(`${ApiRoot}/manga?title=${name}&offset=${offset}&limit=20`);
 	if (res.status === 429) {
 		await RemoveToken(1);
 		return await SearchManga(name, offset);
